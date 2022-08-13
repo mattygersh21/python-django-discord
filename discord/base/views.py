a25c01f292a1a90cfa0ac41ba697e2fee7af5adc
Matthew Gerszewski <dakotaseia@hotmail.com>
2022-08-13 12:06:46 -0500

    * Added ability to delete a message from a room. * Tidied two errors.

--- discord/base/views.py
+++ discord/base/views.py
@@ -136,3 +136,16 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete this message.')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=message.room.id)
+
+    return render(request, 'base/delete.html', {'obj':message})
